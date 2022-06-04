9e39c3c0f4f7ae9293d8c366d3409f38afab4ef6
AbrorIkromov <95936145+AbrorIkromov@users.noreply.github.com>
2022-06-04 23:15:31 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,7 @@
             $('.custom-color-3').removeAttr('id','none')
             $('.custom-color-4').removeAttr('id','none')
             $('.nav-target').addClass('fixed-top navbar-light bg-light');
+            $('#nav-logo').css('visibility', 'visible')
         }
         else{
             $('.custom-color-1').attr('id','custom-color')
@@ -17,13 +18,15 @@
             $('.custom-color-3').attr('id','custom-color')
             $('.custom-color-4').attr('id','custom-color')
             $('.nav-target').removeClass('fixed-top navbar-light bg-light')
+            $('#nav-logo').css('visibility', 'hidden')
         }
     });
 } else {
     $(window).scroll(function () {
         return;
     })
-    $('.nav-target').addClass('fixed-top navbar-light bg-light')
+    $('.nav-target').addClass('fixed-top navbar-light bg-light');
+    $('#nav-logo').css('visibility', 'visible')
 }
 
     // OWL 
