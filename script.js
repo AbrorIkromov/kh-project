0365f20a59cdcfa48eda1bba539a7fd3d768d7cc
AbrorIkromov <95936145+AbrorIkromov@users.noreply.github.com>
2022-07-31 14:53:29 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,29 +4,29 @@
     console.log(screenWidth)
     if (screenWidth > 991.97) {
     $(window).scroll(function () { 
-        if($(document).scrollTop() > 80){
-            $('.custom-color-1').removeAttr('id','none')
-            $('.custom-color-2').removeAttr('id','none')
-            $('.custom-color-3').removeAttr('id','none')
-            $('.custom-color-4').removeAttr('id','none')
-            $('.nav-target').addClass('fixed-top navbar-light bg-light');
-            $('#nav-logo').css('visibility', 'visible')
-        }
-        else{
-            $('.custom-color-1').attr('id','custom-color')
-            $('.custom-color-2').attr('id','custom-color')
-            $('.custom-color-3').attr('id','custom-color')
-            $('.custom-color-4').attr('id','custom-color')
-            $('.nav-target').removeClass('fixed-top navbar-light bg-light')
-            $('#nav-logo').css('visibility', 'hidden')
-        }
+        // if($(document).scrollTop() > 80){
+        //     $('.custom-color-1').removeAttr('id','none')
+        //     $('.custom-color-2').removeAttr('id','none')
+        //     $('.custom-color-3').removeAttr('id','none')
+        //     $('.custom-color-4').removeAttr('id','none')
+        //     $('.nav-target').addClass('fixed-top navbar-light bg-light');
+        //     $('#nav-logo').css('visibility', 'visible')
+        // }
+        // else{
+        //     $('.custom-color-1').attr('id','custom-color')
+        //     $('.custom-color-2').attr('id','custom-color')
+        //     $('.custom-color-3').attr('id','custom-color')
+        //     $('.custom-color-4').attr('id','custom-color')
+        //     $('.nav-target').removeClass('fixed-top navbar-light bg-light')
+        //     $('#nav-logo').css('visibility', 'visible')
+        // }
     });
 } else {
-    $(window).scroll(function () {
-        return;
-    })
-    $('.nav-target').addClass('fixed-top navbar-light bg-light');
-    $('#nav-logo').css('visibility', 'visible')
+    // $(window).scroll(function () {
+    //     return;
+    // })
+    // $('.nav-target').addClass('fixed-top navbar-light bg-light');
+    // $('#nav-logo').css('visibility', 'visible')
 }
 
     // OWL 
